6ea10be312fd6d61c16bc790ec8408a4830c618c
sydneygroverr <sydney.grover8@gmail.com>
2024-03-11 11:42:43 -0600

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+function Welcome() {
+  return <h1>This site calculates the amount of Go his wife has when he is dawdling</h1>;
+}
+
+function GoButton() {
+  // function that increments the number of the page when the button in pressed
+  const incrementGo = () => updateLevel(levelOfGo * 2);
+
+  return (
+    <div>
+      <button onClick={incrementGo}>GO!!!!</button>
+    </div>
+  );
+}
+
+function GoLevel() {
+  return <label>Amount of Go: {levelOfGo}</label>;
+}
+
 function App() {
+  // state variable and statesetter function
+  const [levelOfGo, updateLevel] = useState(1);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome />
+      <br />
+      <br />
+      <GoButton />
     </div>
   );
 }
